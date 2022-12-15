e4dee64a4dc1c706b1c2b35cd675d5509702857d
Emmanuel Ferdman <35470921+emmanuel-ferdman@users.noreply.github.com>
2022-12-15 21:02:41 +0200

    test: add tests for `DoublyLinkedList.keys()` method

--- tests/DoublyLinkedListTest.ts
+++ tests/DoublyLinkedListTest.ts
@@ -3630,6 +3630,88 @@ const doublyLinkedListTest = {
     });
   },
 
+  /** Test "keys" method" **/
+
+  testKeys: function(dsClass) {
+    describe(dsClass.name + ' Base: Test the "keys" method', function() {
+      it('should check "keys" on []', function() {
+        const array = [];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        const arrayIter = array.keys();
+        const dsIter = ds.keys();
+        let arrayCurrent = arrayIter.next();
+        let dsCurrent = dsIter.next();
+        while (!(arrayCurrent.done) && !(dsCurrent.done)) {
+          const arrayValue = arrayCurrent.value;
+          const dsValue = dsCurrent.value;
+          assert.equal(arrayValue,dsValue);
+          arrayCurrent = arrayIter.next();
+          dsCurrent = dsIter.next();
+        }
+        assert.equal(dsIter.done,arrayIter.done);
+      });
+
+      it('should check "keys" on [1]', function() {
+        const array = [1];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        const arrayIter = array.keys();
+        const dsIter = ds.keys();
+        let arrayCurrent = arrayIter.next();
+        let dsCurrent = dsIter.next();
+        while (!(arrayCurrent.done) && !(dsCurrent.done)) {
+          const arrayValue = arrayCurrent.value;
+          const dsValue = dsCurrent.value;
+          assert.equal(arrayValue,dsValue);
+          arrayCurrent = arrayIter.next();
+          dsCurrent = dsIter.next();
+        }
+        assert.equal(dsIter.done,arrayIter.done);
+      });
+
+      it('should check "keys" on [1,2,3]', function() {
+        const array = [1,2,3];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        const arrayIter = array.keys();
+        const dsIter = ds.keys();
+        let arrayCurrent = arrayIter.next();
+        let dsCurrent = dsIter.next();
+        while (!(arrayCurrent.done) && !(dsCurrent.done)) {
+          const arrayValue = arrayCurrent.value;
+          const dsValue = dsCurrent.value;
+          assert.equal(arrayValue,dsValue);
+          arrayCurrent = arrayIter.next();
+          dsCurrent = dsIter.next();
+        }
+        assert.equal(dsIter.done,arrayIter.done);
+      });
+
+      it('should check "keys" on [null,undefined,NaN,{},Infinity,"string",-0,1.5,true,new Date("21 Dec 1997 14:12:00 UTC")]', function() {
+        const array = [null,undefined,NaN,{},Infinity,"string",-0,1.5,true,new Date("21 Dec 1997 14:12:00 UTC")];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        const arrayIter = array.keys();
+        const dsIter = ds.keys();
+        let arrayCurrent = arrayIter.next();
+        let dsCurrent = dsIter.next();
+        while (!(arrayCurrent.done) && !(dsCurrent.done)) {
+          const arrayValue = arrayCurrent.value;
+          const dsValue = dsCurrent.value;
+          assert.equal(arrayValue,dsValue);
+          arrayCurrent = arrayIter.next();
+          dsCurrent = dsIter.next();
+        }
+        assert.equal(dsIter.done,arrayIter.done);
+      });
+    });
+  },
+
   /** Test "map" method" **/
 
   testMap: function(dsClass) {
