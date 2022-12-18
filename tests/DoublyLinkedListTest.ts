7d99d6452b43d9b3dbc29a38de473d3793323d55
emmanuel-ferdman <35470921+emmanuel-ferdman@users.noreply.github.com>
2022-12-18 19:01:56 +0200

    test: add tests for `DoublyLinkedList.pop()` method

--- tests/DoublyLinkedListTest.ts
+++ tests/DoublyLinkedListTest.ts
@@ -4212,6 +4212,57 @@ const doublyLinkedListTest = {
     });
   },
 
+  /** Test "pop" method" **/
+
+  testPop: function(dsClass) {
+    describe(dsClass.name + ' Base: Test the "pop" method', function() {
+      it('should check "pop" on []', function() {
+        const array = [];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.pop(),array.pop());
+        assert(ds.isEqual(array));
+      });
+
+      it('should check "pop" on [1]', function() {
+        const array = [1];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.pop(),array.pop());
+        assert(ds.isEqual(array));
+      });
+
+      it('should check "pop" on [1,2,3]', function() {
+        const array = [1,2,3];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.pop(),array.pop());
+        assert(ds.isEqual(array));
+      });
+
+      it('should check "pop" on [null]', function() {
+        const array = [null];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.pop(),array.pop());
+        assert(ds.isEqual(array));
+      });
+
+      it('should check "pop" on [undefined]', function() {
+        const array = [undefined];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.pop(),array.pop());
+        assert(ds.isEqual(array));
+      });
+    });
+  },
+
   /** Test "push" method" **/
 
   testPush: function(dsClass) {
