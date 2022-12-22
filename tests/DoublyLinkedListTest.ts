8b4d782dc195eec1d87b1ae3024cdbdeb8c2ee17
Emmanuel Ferdman <35470921+emmanuel-ferdman@users.noreply.github.com>
2022-12-22 18:25:54 +0200

    test: add tests for `DoublyLinkedList.toString()` method

--- tests/DoublyLinkedListTest.ts
+++ tests/DoublyLinkedListTest.ts
@@ -6116,6 +6116,60 @@ const doublyLinkedListTest = {
     });
   },
 
+  /** Test "toString" method" **/
+
+  testToString: function(dsClass) {
+    describe(dsClass.name + ' Base: Test the "toString" method', function() {
+      it('should check "toString" of list []', function() {
+        const array = [];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+
+      it('should check "toString" of list [1]', function() {
+        const array = [1];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+
+      it('should check "toString" of list [1,2,3]', function() {
+        const array = [1,2,3];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+
+      it('should check "toString" of list [1,2,3]', function() {
+        const array = [1,2,3];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+
+      it('should check "toString" of list [1,-2,3]', function() {
+        const array = [1,-2,3];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+
+      it('should check "toString" of list [null,undefined,NaN,{},Infinity,"string",-0,1.5,true,new Date("21 Dec 1997 14:12:00 UTC")]', function() {
+        const array = [null,undefined,NaN,{},Infinity,"string",-0,1.5,true,new Date("21 Dec 1997 14:12:00 UTC")];
+        const ds = new dsClass(false,array);
+        assert.equal(ds.length,array.length);
+        assert(ds.isEqual(array));
+        assert.equal(ds.toString(),array.toString());
+      });
+    });
+  },
+
   /** Test "unshift" method" **/
 
   testUnshift: function(dsClass) {
