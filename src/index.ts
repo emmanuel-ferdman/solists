96c2aacd33937f65b36bdf3a6dd2c073433fb98d
emmanuel-ferdman <35470921+emmanuel-ferdman@users.noreply.github.com>
2023-01-04 17:20:08 +0200

    feat: add the entry point for the project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { FrequencyCountSoList } from './FrequencyCountSoList';
+import { MoveToFrontSoList } from './MoveToFrontSoList';
+import { TransposeSoList } from './TransposeSoList';
+
+export {
+  FrequencyCountSoList,
+  MoveToFrontSoList,
+  TransposeSoList
+};
